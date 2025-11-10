403aeec7c6f2b1b0612f9039608d1c2fe919685b
expo <expoworld112@gmail.com>
2025-11-10 16:35:47 +0530

    u2

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// export const FRONTEND = "https://frontend-omega-eight-80.vercel.app/";
+export const FRONTEND = "https://coding4u-frontend.vercel.app";
 
-export const FRONTEND = "http://localhost:3000";
-export const API = process.env.API || 'http://localhost:8000';
+// export const FRONTEND = "http://localhost:3000";
+// export const API = process.env.API || 'http://localhost:8000';
+export const API = process.env.API || 'https://expo-back.vercel.app';
