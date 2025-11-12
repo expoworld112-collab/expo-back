721c7b8c1daf6c5eb4aff4df2ee55a8d6b2325e9
expo <expoworld112@gmail.com>
2025-11-12 08:18:55 +0530

    cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end(); // handle preflight instantly
+    return res.status(200).end(); // handle preflight 
   }
 
   next();
