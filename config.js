5922737f3557d5a96d545b96c4b4a64df92f6032
expo <expoworld112@gmail.com>
2025-11-13 14:54:16 +0530

    p

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const FRONTEND = "https://expo-front-q575.vercel.app/";
 
 // export const FRONTEND = "http://localhost:3000";
 // export const API = process.env.API || 'http://localhost:8000';
-export const API = process.env.API || 'https://expo-back.vercel.app';
+export const API = process.env.API || 'https://expo-back.vercel.app/api';
